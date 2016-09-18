fa4d5441bcd8f1542fdbd2ebabd82043d029677e
TobiahRex <tobiah.rex@gmail.com>
2016-09-18 15:08:51 -0700

    added Expirment folder

--- QuickSort.js
+++ QuickSort.js
@@ -3,7 +3,6 @@ function swap (items, firstIndex, secondIndex) {
   items[firstIndex] = items[secondIndex]
   items[secondIndex] = temp
 }
-
 function partition (items, left, right) {
   let pivot = items[Math.floor((right + left) / 2)]  // pivot value is middle item
   let i = left     // starts from left and goes right to pivot index
@@ -12,17 +11,12 @@ function partition (items, left, right) {
   // while the two indices don't match
   while (i <= j) {
     // if the item on the left is less than the pivot, continue right
-    while (items[i] < pivot) {
-      i++
-    }
+    while (items[i] < pivot) i++
     // if the item on the right is greater than the pivot, continue left
-    while (items[j] > pivot) {
-      j--
-    }
+    while (items[j] > pivot) j--
     // if the two indices still don't match, swap the values
     if (i <= j) {
       swap(items, i, j)
-
       // change indices to continue loop
       i++
       j--
@@ -31,62 +25,25 @@ function partition (items, left, right) {
   // this value is necessary for recursion
   return i
 }
-
-
-/**
-* A quicksort implementation in JavaScript. The array
-* is sorted in place.
-* @param {Array} items An array of items to sort.
-* @return {Array} The sorted array.
-*/
 function quickSort (items, left, right) {
   var index
-
   // performance - don't sort an array with zero or one items
   if (items.length > 1) {
     // fix left and right values - might not be provided
     left = typeof left !== 'number' ? 0 : left
     right = typeof right !== 'number' ? items.length - 1 : right
-
     // split up the entire array
     index = partition(items, left, right)
-
     // if the returned index
     if (left < index - 1) {
       quickSort(items, left, index - 1)
     }
-
     if (index < right) {
       quickSort(items, index, right)
     }
   }
   return items
 }
-
-function quickSort(arr, left, right) {
-  let pivotIndex
-  let n = arr.length
-  if (n > 1) {
-    left = typeof left !== 'number' ? 0 : left
-    right = typeof right !== 'number' ? n - 1 : right
-    pivotIndex = partition(arr, left, right)
-    if (left < pivotIndex - 1){
-      quickSort(arr, left, pivotIndex - 1)
-    }
-    if (right > pivotIndex + 1){
-      quickSort(arr, pivotIndex + 1, right)
-    }
-
-
-
-
-
-
-
-  }
-}
-
-
 
 // function swap (array, s, e) {
 //   let item = array[s]
---
+++ jsExpirments.js
@@ -0,0 +1,61 @@
+// function x(){
+//   const hi = 'hi from x'
+//   console.log(hi)
+// }
+//
+// function y(){
+//   const hi = 'hi from y'
+//   console.log(hi);
+// }
+// x()
+// y()
+
+// function z() {
+//     const hi = 'hi from z'
+//     console.log(hi);
+//
+//   let i = 0
+//   while(i < 2) {
+//     i++
+//     const hi = 'hi from while loop'
+//     console.log(hi);
+//   }
+// }
+// z()
+
+// function switchEx(number) {
+//   switch(number) {
+//     case 0: {
+//       const hi = 'hi from case 0'
+//       console.log(hi)
+//     }; break;
+//     case 1: {
+//       const hi = 'hi from case 1'
+//       console.log(hi)
+//     }; break;
+//   }
+// }
+// switchEx(1)
+
+// function varExp() {
+//   var x = 'hi'
+//
+//   console.log(x)
+//   for(var x = 0; x < 2; x++){
+//
+//     console.log(x)
+//   }
+// }
+// varExp()
+
+
+// let x = function() { console.log('function this: ', this)}
+// let x = () => console.log('function this: ', this)
+// x()
+
+let obj = {
+  a: 1,
+  b: 2
+}
+
+let myObj = obj
