72599abebba81b8da33f82352642f0e57f7e242b
TobiahRex <tobiah.rex@gmail.com>
2016-10-27 10:52:57 -0700

    added another fibRecursive answer .

---
+++ recursion/fibRecursive.js
@@ -0,0 +1,75 @@
+// function fib(n) {
+//   /*
+//     could count downwards with n.
+//     could count up towards n.
+//
+//     start with fibonacci previous, current, next.
+//     previous + next = current;
+//     current start out at 0.
+//     next start out as current + current.
+//     previous starts out at 0, but is the sum of the previous two.
+//
+//     doing this recursively...
+//
+//     we would need to pass a counter to fibRecursive, and determined inside that functions base case, if we needed to calculate again.
+//
+//     base case.
+//
+//     if (n !== 0);
+//
+//     passing in a decremented counter each time.
+//   */
+//   const fibRecursive = (target, previous, current) => {
+//     let sum = 0;
+//     // if we need to call it again
+//     if (target !== 0) {
+//       // calculate the new total.
+//       sum += (previous + current);
+//       // pass in the previous total, with the new total, and call it again.
+//       return fibRecursive(--target, current, sum);
+//       // if we don't need to call again. (target === 0)
+//     } else {
+//       // then we can retun the total and be done.
+//       return (current);
+//     }
+//   }
+//
+//   if (n > 0) return fibRecursive(--n, 0, 1);  // if n is a number, find answer.
+//   return 0;
+// }
+
+// // RECURSIVE (mine)
+// function fib(n) {
+//   function fibRecursive(target, previous, current) {
+//     if (target !== 0) return fibRecursive(--target, current, (previous + current));
+//     return current;
+//   }
+//   if (n > 0) return fibRecursive(--n, 0, 1);
+//   return 0;
+// }
+
+// RECURSIVE (Michael)
+function fib(n) {
+  if (n < 1) return 0;
+  if (n === 1) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+// // ITERATIVE
+// function fib(n) {
+//   let previous = 1;
+//   let current = 0;
+//   let next = 0;
+//   let i = n;
+//
+//   while(i--){
+//     next = previous + current;
+//     previous = current;
+//     current = next;
+//   }
+//
+//   if (n > 0) return current;
+//   return 0;
+// }
+
+console.log('>>> ANSWER: ', fib(21)); // 21st = 10946
