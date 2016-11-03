690e861298083b357d228ffae7ed54b32dbeb4d9
TobiahRex <tobiah.rex@gmail.com>
2016-11-02 22:45:14 -0700

    added BST description in the comments.

--- BinaryTree/binaryTree.js
+++ BinaryTree/binaryTree.js
@@ -1,5 +1,38 @@
 const customUtil = require('./utils');
 
+/*
+  Binary search trees (BST)'s use left, right, and parent pointers in memory to connect data together.  The class itself keeps track of a SINGLE array containg the actual data values of each array.  The order that the data appears in the array is not arbitrary however.  The order of the tree, must be in order.  For example, values right of the parent are greater than the parent, and vice versa.
+
+  BST data array...
+  [1,8,4,5,2,3,6]
+  to retrieve key 0's left and right nodes data, you use the following formula.
+  left-node-data = 2i + 1
+  right-node-data = 2i + 2
+
+  therefore the left node data to key 0 === 8
+  & the right node data to key 0 === 4
+
+  for node at key 1, it's 5 & 2.
+  for ndoe at key 2, it's 3 & 6.
+  etc...
+
+  There are MANY utitlity functions for BST's.
+  - maxHeight.
+  - maxNodes.
+  - current height.
+  - current Node count.
+  - checking all nodes for condition
+  - checking all nodes are in order
+  - creating a new node at the left
+  - creating a new node at the right
+  - inserting a new node at the left, or right based on it's value.
+  - delete a new node and re-adjusting the pointers in the tree to replace the missing node, and then also re-balancing the tree if it's a "self balancing" BST.
+
+  The reason BST's are so valuable is because they have O(logn) lookup time when they're balanced, and O(n) when they are unbalanced.  They typically have O(logn) time complexity on all other related common methods: search, insert, delete, access.  When factoring in time complexity, the height of the tree is extremely relevant, and whether the tree is "Perfectly" balanced, or "Complete" is also important information.
+
+  BST's cannot have a height difference between levels of more than 1.  The height is calculated by | left node - (right node)|.  If it's greater than 1, then there is an imbalance and it must be corrected.
+*/
+
 // skip to line 160 to see work methods
 class BinarySearchTree {
   constructor(options) {
