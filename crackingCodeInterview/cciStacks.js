425e48bca63c8057eae4c71b962a3a66e852635b
TobiahRex <tobiah.rex@gmail.com>
2016-11-30 14:55:48 -0800

    added helper function "popCheck" to cciStacks.js

--- crackingCodeInterview/cciStacks.js
+++ crackingCodeInterview/cciStacks.js
@@ -1,39 +1,39 @@
 /* Balanced Brackets
-  A matching pair of brackets is not balanced if
-  the set of brackets it encloses are not matched.
-  For example, {[(])} is not balanced because
-  the contents in between { and } are not balanced.
-  The pair of square brackets encloses a single,
-  unbalanced opening bracket,
-  (, and the pair of parentheses encloses a single,
-  unbalanced closing square bracket, ].
+A matching pair of brackets is not balanced if
+the set of brackets it encloses are not matched.
+For example, {[(])} is not balanced because
+the contents in between { and } are not balanced.
+The pair of square brackets encloses a single,
+unbalanced opening bracket,
+(, and the pair of parentheses encloses a single,
+unbalanced closing square bracket, ].
 
-  By this logic,
-  we say a sequence of brackets is considered to be balanced
-  if the following conditions are met:
+By this logic,
+we say a sequence of brackets is considered to be balanced
+if the following conditions are met:
 
-  It contains no unmatched brackets.
-  The subset of brackets enclosed
-  within the confines of a matched pair of brackets
-  is also a matched pair of brackets.
+It contains no unmatched brackets.
+The subset of brackets enclosed
+within the confines of a matched pair of brackets
+is also a matched pair of brackets.
 
-  Given  strings of brackets,
-  determine whether each sequence of brackets is balanced.
-  If a string is balanced,
-  print YES on a new line;
-  otherwise,
-  print NO on a new line.
+Given  strings of brackets,
+determine whether each sequence of brackets is balanced.
+If a string is balanced,
+print YES on a new line;
+otherwise,
+print NO on a new line.
 
-  Sample Input:
-  3
-  {[()]}
-  {[(])}
-  {{[[(())]]}}
+Sample Input:
+3
+{[()]}
+{[(])}
+{{[[(())]]}}
 
-  Smaple Output:
-  YES
-  NO
-  YES
+Smaple Output:
+YES
+NO
+YES
 */
 
 // Step 1 - Gather all the information.
@@ -41,25 +41,23 @@
 class Stack {
   constructor() {
     this.top = null;
-    this.createNode = this.createNode.bind(this);
-    this.push = this.push.bind(this);
-    this.pop = this.pop.bind(this);
   }
 
-  createNode(data) {
+  createNode(direction, type) {
     let node = {
-      data,
+      direction,
+      type,
       next: null
     }
     return node;
   }
 
-  push(data) {
-    let node = this.createNode(data);
+  push(direction, type) {
+    let node = this.createNode(direction, type);
     if (this.top === null) {
       this.top = node;
     } else {
-      node.next = top;
+      node.next = this.top;
       this.top = node;
     }
   }
@@ -73,6 +71,33 @@ class Stack {
       return oldTop;
     }
   }
+}
+const stack = new Stack;
+
+function popCheck(t) {
+  let { direction, type } = stack.pop();
+
+  if ((direction !== 'right') && (type !== t)) return true
+  return false;
+}
+
+function main(n) {
+  let str = n[1]
+  let results = '';
+  if ((str[0] === '}') || (str[0] === ')') || (str[0] === ']')) {
+    results += 'NO\n';
+  }
+  for (let i = 0; i < str.length; i++) {
+    console.log('str[i]', str[i]);
+    switch(str[i]) {
+      case '{': { stack.push('right', 'curly') } break;
+      case '[': { stack.push('right', 'square') } break;
+      case '(': { stack.push('right', 'parens') } break;
+      case ')': { popCheck('parens') } break;
+    }
+  }
+  console.log('stack.top \n',  stack.top);
+  return results;
 }
 
 let testCases = [1,'{[()]}'];
