9294b3244d92aedbe11cad395f0625647e927c3e
TobiahRex <tobiah.rex@gmail.com>
2017-03-13 15:44:07 +0800

    small edit to fibIterative.

--- recursion/fibIterative.js
+++ recursion/fibIterative.js
@@ -1,18 +1,22 @@
-// ITERATIVE
+/* NOTE:
+  Find's the n'th Fibonnaci digit iteratively.
+*/
 function fib(n) {
-  let previous = 1;
-  let current = 0;
-  let next = 0;
-  let i = n;
+  let previous = 1,
+      current = 0,
+      next = 0,
+      i = n;
 
   while(i--){
     next = previous + current;
     previous = current;
     current = next;
   }
-
-  if (n > 0) return current;
-  return 0;
+  return (n === 0 ? 0 : current);
 }
 
 console.log('>>> ANSWER: ', fib(21)); // 21st = 10946
+// 8, 13
+// next = 21
+// prev = 13
+//
