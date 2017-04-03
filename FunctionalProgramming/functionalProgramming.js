3829b14385dba9d95eb973b94d06654e514c720f
TobiahRex <tobiah.rex@gmail.com>
2017-04-04 07:46:20 +0900

    finished Excercise 11.

---
+++ FunctionalProgramming/functionalProgramming.js
@@ -0,0 +1,163 @@
+/*
+Querying Trees:
+
+Sometimes, in addition to flat arrays, we need to query trees. Trees pose a challenge because we need to flatten them into arrays in order to apply filter() and map() operations on them. In this section we'll define a concatAll() function that we can combine with map() and filter() to query trees.
+
+Exercise 9: Flatten the movieLists array into an array of video ids
+
+Let's start by using two nested forEach loops to collect the id of every video in the two-dimensional movieLists array.
+*/
+
+function queryingTrees() {
+	var movieLists = [
+		{
+			name: "New Releases",
+			videos: [
+				{
+					"id": 70111470,
+					"title": "Die Hard",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 654356453,
+					"title": "Bad Boys",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		},
+		{
+			name: "Dramas",
+			videos: [
+				{
+					"id": 65432445,
+					"title": "The Chamber",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 675465,
+					"title": "Fracture",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		}
+	],
+	allVideoIdsInMovieLists = [];
+
+	// ------------   INSERT CODE HERE!  -----------------------------------
+	// Use two nested forEach loops to flatten the movieLists into a list of
+	// video ids.
+	movieLists.forEach((list) => {
+		list.videos.forEach((movie) => {
+			allVideoIdsInMovieLists.push(movie.id);
+		});
+	});
+	// ------------   INSERT CODE HERE!  -----------------------------------
+
+	return allVideoIdsInMovieLists;
+
+}
+// -----------------------------------------------------------------------------
+/*
+Flattening trees with nested forEach expressions is easy because we can explicitly add items to the array. Unfortunately it's exactly this type of low-level operation that we've been trying to abstract away with functions like map() and filter(). Can we define a function that's abstract enough to express our intent to flatten a tree, without specifying too much information about how to carry out the operation?
+
+Exercise 10: Implement concatAll()
+
+Let's add a concatAll() function to the Array type. The concatAll() function iterates over each sub-array in the array and collects the results in a new, flat array. Notice that the concatAll() function expects each item in the array to be another array.
+*/
+
+Array.prototype.concatAll = function() {
+	var results = [];
+	this.forEach(function(subArray) {
+		// ------------ INSERT CODE HERE! ----------------------------
+		// Add all the items in each subArray to the results array.
+		results = results.concat(subArray);
+		// ------------ INSERT CODE HERE! ----------------------------
+	});
+
+	return results;
+};
+
+// JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll()) === "[1,2,3,4,5,6,7,8,9]"
+// [1,2,3].concatAll(); // throws an error because this is a one-dimensional array
+// console.log([[1,2,3],[4,5,6],[7,8,9]].concatAll());
+
+
+// -----------------------------------------------------------------------------
+/*
+concatAll is a very simple function, so much so that it may not be obvious yet how it can be combined with map() to query a tree. Let's try an example...
+
+Exercise 11: Use map() and concatAll() to project and flatten the movieLists into an array of video ids
+
+Hint: use two nested calls to map() and one call to concatAll().
+*/
+function exc11() {
+	var movieLists = [
+			{
+				name: "New Releases",
+				videos: [
+					{
+						"id": 70111470,
+						"title": "Die Hard",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"bookmark": []
+					},
+					{
+						"id": 654356453,
+						"title": "Bad Boys",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"bookmark": [{ id: 432534, time: 65876586 }]
+					}
+				]
+			},
+			{
+				name: "Dramas",
+				videos: [
+					{
+						"id": 65432445,
+						"title": "The Chamber",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"bookmark": []
+					},
+					{
+						"id": 675465,
+						"title": "Fracture",
+						"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"bookmark": [{ id: 432534, time: 65876586 }]
+					}
+				]
+			}
+		];
+
+	// ------------   INSERT CODE HERE!  -----------------------------------
+	// Use map and concatAll to flatten the movieLists in a list of video ids.
+	movieLists = movieLists
+	.map((list) => list.videos.map((video) => video.id))
+	.concatAll();
+	// ------------   INSERT CODE HERE!  -----------------------------------
+
+	return movieLists // Complete this expression!
+}
+
+
+
+console.log(exc11());
