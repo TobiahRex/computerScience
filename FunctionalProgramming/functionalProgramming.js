8d8e5c35774d5ff74e513a234ae648515dd5639b
TobiahRex <tobiah.rex@gmail.com>
2017-04-04 13:46:54 +0900

    added notes for excercise 14.

--- FunctionalProgramming/functionalProgramming.js
+++ FunctionalProgramming/functionalProgramming.js
@@ -104,49 +104,49 @@ Hint: use two nested calls to map() and one call to concatAll().
 */
 function exc11() {
 	var movieLists = [
-			{
-				name: "New Releases",
-				videos: [
-					{
-						"id": 70111470,
-						"title": "Die Hard",
-						"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
-						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-						"rating": 4.0,
-						"bookmark": []
-					},
-					{
-						"id": 654356453,
-						"title": "Bad Boys",
-						"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
-						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-						"rating": 5.0,
-						"bookmark": [{ id: 432534, time: 65876586 }]
-					}
-				]
-			},
-			{
-				name: "Dramas",
-				videos: [
-					{
-						"id": 65432445,
-						"title": "The Chamber",
-						"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
-						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-						"rating": 4.0,
-						"bookmark": []
-					},
-					{
-						"id": 675465,
-						"title": "Fracture",
-						"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
-						"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-						"rating": 5.0,
-						"bookmark": [{ id: 432534, time: 65876586 }]
-					}
-				]
-			}
-		];
+		{
+			name: "New Releases",
+			videos: [
+				{
+					"id": 70111470,
+					"title": "Die Hard",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 654356453,
+					"title": "Bad Boys",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		},
+		{
+			name: "Dramas",
+			videos: [
+				{
+					"id": 65432445,
+					"title": "The Chamber",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 675465,
+					"title": "Fracture",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		}
+	];
 
 	// ------------   INSERT CODE HERE!  -----------------------------------
 	// Use map and concatAll to flatten the movieLists in a list of video ids.
@@ -181,109 +181,277 @@ Furthermore, you're not allowed to use indexers in any of the remaining exercise
 */
 function exc12() {
 	var movieLists = [
-			{
-				name: "Instant Queue",
-				videos : [
-					{
-						"id": 70111470,
-						"title": "Die Hard",
-						"boxarts": [
-							{
-								width: 150,
-								height: 200,
-								url: "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg"
-							},
-							{
-								width: 200,
-								height: 200,
-								url: "http://cdn-0.nflximg.com/images/2891/DieHard200.jpg"
-							}
-						],
-						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
-						"rating": 4.0,
-						"bookmark": []
-					},
-					{
-						"id": 654356453,
-						"title": "Bad Boys",
-						"boxarts": [
-							{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
-							{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" }
-
-						],
-						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
-						"rating": 5.0,
-						"bookmark": [{ id: 432534, time: 65876586 }]
-					}
-				]
-			},
-			{
-				name: "New Releases",
-				videos: [
-					{
-						"id": 65432445,
-						"title": "The Chamber",
-						"boxarts": [
-							{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
-							{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
-						],
-						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
-						"rating": 4.0,
-						"bookmark": []
-					},
-					{
-						"id": 675465,
-						"title": "Fracture",
-						"boxarts": [
-							{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
-							{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
-							{ width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
-						],
-						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
-						"rating": 5.0,
-						"bookmark": [{ id: 432534, time: 65876586 }]
-					}
-				]
-			}
-		];
-
-		movieLists = movieLists
-		.map((list) =>
-			list.videos.map((video) =>
-				({
-					id: video.id,
-					title: video.title,
-					boxart: video.boxarts,
-				})
-			)
-		)
-		.concatAll()
-		.map((video) => {
-			let newBoxArt = '';
-			video.boxart
-			.filter((art) => (art.height === 200 && art.width === 150))
-			.map((attr) => {
-				newBoxArt = attr.url;
-			})
-			video.boxart = newBoxArt;
-			return video;
-		});
-
-	// Use one or more map, concatAll, and filter calls to create an array with the following items
-	/*
-	[
 		{
-			"id": 675465,
-			"title": "Fracture",
-			"boxart":"http://cdn-0.nflximg.com/images/2891/Fracture150.jpg"
-		}, {
-		"id": 65432445,"title": "The Chamber","boxart":"http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
-		{"id": 654356453,"title": "Bad Boys","boxart":"http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" },
-		{"id": 70111470,"title": "Die Hard","boxart":"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
+			name: "Instant Queue",
+			videos : [
+				{
+					"id": 70111470,
+					"title": "Die Hard",
+					"boxarts": [
+						{
+							width: 150,
+							height: 200,
+							url: "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg"
+						},
+						{
+							width: 200,
+							height: 200,
+							url: "http://cdn-0.nflximg.com/images/2891/DieHard200.jpg"
+						}
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 654356453,
+					"title": "Bad Boys",
+					"boxarts": [
+						{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+						{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" }
+
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		},
+		{
+			name: "New Releases",
+			videos: [
+				{
+					"id": 65432445,
+					"title": "The Chamber",
+					"boxarts": [
+						{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+						{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 675465,
+					"title": "Fracture",
+					"boxarts": [
+						{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+						{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+						{ width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		}
 	];
+
+	movieLists = movieLists
+	.map((list) =>
+	list.videos.map((video) =>
+	({
+		id: video.id,
+		title: video.title,
+		boxart: video.boxarts,
+	})
+)
+)
+.concatAll()
+.map((video) => {
+	let newBoxArt = '';
+	video.boxart
+	.filter((art) => (art.height === 200 && art.width === 150))
+	.map((attr) => {
+		newBoxArt = attr.url;
+	})
+	video.boxart = newBoxArt;
+	return video;
+});
+
+// Use one or more map, concatAll, and filter calls to create an array with the following items
+/*
+[
+{
+"id": 675465,
+"title": "Fracture",
+"boxart":"http://cdn-0.nflximg.com/images/2891/Fracture150.jpg"
+}, {
+"id": 65432445,"title": "The Chamber","boxart":"http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+{"id": 654356453,"title": "Bad Boys","boxart":"http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" },
+{"id": 70111470,"title": "Die Hard","boxart":"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
+];
+*/
+
+return movieLists // Complete this expression!
+
+}
+// console.log(exc12());
+// -----------------------------------------------------------------------------
+/*
+Fantastic job! Now you've learned to use concatAll() alongside map() and filter() to query trees. Notice that map() and concatAll() are very commonly chained together. Let's create a small helper function to help us with this common pattern.
+
+Exercise 13: Implement concatMap()
+
+Nearly every time we flatten a tree we chain map() and concatAll(). Sometimes, if we're dealing with a tree several levels deep, we'll repeat this combination many times in our code. To save on typing, let's create a concatMap function that's just a map operation, followed by a concatAll.
+*/
+
+Array.prototype.concatMap = function(projectionFunctionThatReturnsArray) {
+	return this.
+	map(function(item) {
+		// ------------   INSERT CODE HERE!  ----------------------------
+		// Apply the projection function to each item. The projection
+		// function will return a new child array. This will create a
+		// two-dimensional array.
+		return projectionFunctionThatReturnsArray(item);
+		// ------------   INSERT CODE HERE!  ----------------------------
+	}).
+	// apply the concatAll function to flatten the two-dimensional array
+	concatAll();
+};
+
+/*
+var spanishFrenchEnglishWords = [ ["cero","rien","zero"], ["uno","un","one"], ["dos","deux","two"] ];
+// collect all the words for each number, in every language, in a single, flat list
+var allWords = [0,1,2].
+concatMap(function(index) {
+return spanishFrenchEnglishWords[index];
+});
+
+return JSON.stringify(allWords) === '["cero","rien","zero","uno","un","one","dos","deux","two"]';
+*/
+var spanishFrenchEnglishWords = [ ["cero","rien","zero"], ["uno","un","one"], ["dos","deux","two"] ];
+// console.log([0,1,2].concatMap((index) => spanishFrenchEnglishWords[index]));
+// -----------------------------------------------------------------------------
+/*
+Now, instead of using map().concatAll() to flatten a tree, we can just use concatMap helper function.
+
+Exercise 14: Use concatMap() to retrieve id, title, and 150x200 box art url for every video
+
+Let's repeat the exercise we just performed. However this time we'll simplify the code by replacing the map().concatAll() calls with concatMap().
+*/
+function exc14() {
+	var movieLists = [
+		{
+			name: "Instant Queue",
+			videos : [
+				{
+					"id": 70111470,
+					"title": "Die Hard",
+					"boxarts": [
+						{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+						{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 654356453,
+					"title": "Bad Boys",
+					"boxarts": [
+						{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+						{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" }
+
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		},
+		{
+			name: "New Releases",
+			videos: [
+				{
+					"id": 65432445,
+					"title": "The Chamber",
+					"boxarts": [
+						{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+						{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 675465,
+					"title": "Fracture",
+					"boxarts": [
+						{ width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+						{ width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+						{ width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+					],
+					"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		}
+	];
+	// My Initial Answer
+	// return movieLists.concatMap(({ videos }) =>
+	// 	videos.map(({ id, title, boxarts }) =>
+	// 		({
+	// 			id,
+	// 			title,
+	// 			boxart: boxarts,
+	// 		})
+	// 	))
+	// 	.map((video) => {
+	// 		let newBoxArt = ''
+	// 		video.boxart = video.boxart
+	// 		.filter(({ width, height }) => (width === 150 && height === 200))
+	// 		.map(({ url }) => {
+	// 			newBoxArt = url;
+	// 		})
+	// 		video.boxart = newBoxArt;
+	// 		return video;
+	// 	});
+
+	/*
+	Their Answer
+	- Something that was not clear to me with before studying their answer was thinking of concatMap as a pattern that takes a nested data structure from an inner layer, and moves it to an outter layer.  The map is there to be the iterator, but the concat is there to actually do the moving between layers.  So in the future, when you need to move a nested data structure to an upper level, this would be the patter.  Everything else in their answer, is simply there to manipulate the data that we'll eventually move...nothing else.
 	*/
+
+	return movieLists.concatMap(({ videos }) =>
+		videos.concatMap((video) =>
+			video.boxarts
+			.filter(({ width }) => width === 150)
+			.map((boxart) =>
+			({ id: video.id, title: video.title, boxart: boxart.url })
+			);
+		);
+	);
+	// movieLists = movieLists.concatMap(function(movieList) {
+	//
+	// 	let newMovieList = movieList.videos.concatMap(function(video) {
+	//
+	// 		let newVideo = video.boxarts
+	// 		.filter(function(boxart) {
+	// 			return boxart.width === 150;
+	// 		})
+	// 		.map(function(boxart) {
+	// 			return {id: video.id, title: video.title, boxart: boxart.url};
+	// 		});
+	// 		// console.log('newVideo: ', newVideo);
+	// 		return newVideo
+	// 	});
+	// 	console.log('newMovieList: ', newMovieList);
+	// 	return newMovieList;
+	// });
+	// console.log('movieLists: ', movieLists);
+
+	// Use one or more concatMap, map, and filter calls to create an array with the following items
+	// [
+	//	 {"id": 675465, "title": "Fracture", "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+	//	 {"id": 65432445, "title": "The Chamber", "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+	//	 {"id": 654356453, "title": "Bad Boys", "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" },
+	//	 {"id": 70111470, "title": "Die Hard", "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" }
+	// ];
 
 	return movieLists // Complete this expression!
 
 }
-console.log(exc12());
+// console.log(exc14());
+exc14();
