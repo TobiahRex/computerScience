ed68dee4f31e8b3d542fbd52fd5aeb8ec0936614
TobiahRex <tobiah.rex@gmail.com>
2017-05-28 20:24:16 +0900

    created tally example.

--- reduce/simple.js
+++ reduce/simple.js
@@ -3,3 +3,28 @@ function initialValue() {
   console.log(data.reduce((a, b) => a + b, 10));
 }
 // initialValue();
+
+function tally() {
+  const data = [
+    'angular',
+    'angular',
+    'angular',
+    'react',
+    'react',
+    'react',
+    'react',
+    'ember',
+    'vanilla',
+    'vanilla',
+  ];
+  const results = data.reduce((accum, next) => {
+    if (accum[next]) {
+      accum[next] += 1;
+      return accum;
+    }
+    accum[next] = 1;
+    return accum;
+  }, {});
+  console.log(JSON.stringify(results, null, 2));
+}
+tally();
