77249e93b1a8ab13852d5665bb502d9c0ac3c0b9
TobiahRex <tobiah.rex@gmail.com>
2017-05-28 21:15:31 +0900

    compose complete.

--- reduce/compose.js
+++ reduce/compose.js
@@ -1,12 +1,17 @@
-const increment = value => value + 1;
-const decrement = value => value - 1;
-const double = value => value * 2;
-const halve = value => value / 2;
+const increment = value => (value + 1);
+const decrement = value => (value - 1);
+const double = value => (value * 2);
+const halve = value => (value / 2);
 
 const result = [
   increment,
+  increment,
+  increment,
   decrement,
   double,
+  double,
+  double,
   halve,
-].reduce((acc, next) => next(acc), 10);
+].reduce((acc, next) => next(acc), 1);
+
 console.log(result);
