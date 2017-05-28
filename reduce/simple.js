e8acd5603313c4c46c4daeb9c11dddde0227501b
TobiahRex <tobiah.rex@gmail.com>
2017-05-28 20:51:38 +0900

    creating flatMap operation.

--- reduce/simple.js
+++ reduce/simple.js
@@ -18,13 +18,63 @@ function tally() {
     'vanilla',
   ];
   const results = data.reduce((accum, next) => {
-    if (accum[next]) {
-      accum[next] += 1;
-      return accum;
-    }
-    accum[next] = 1;
+    if (accum[next]) accum[next] += 1
+    else accum[next] = 1;
     return accum;
   }, {});
   console.log(JSON.stringify(results, null, 2));
 }
-tally();
+// tally();
+function flatten() {
+  const data = [[1,2,3], [4,5,6], [7,8,9]];
+  const result = data.reduce((accum, next) => {
+    return accum.concat(next);
+  }, [])
+  console.log(result);
+}
+// flatten();
+function flatMap() {
+  const data = [
+    {
+      title: 'Batman-Begins',
+      year: 2006,
+      cast: [
+        'Christian Bale',
+        'Michael Caine',
+        'Liam Neeson',
+        'Katie Holmes',
+        'Gary Oldman',
+        'Cilian Murphy',
+      ],
+    },
+    {
+      title: 'The Dark Knight',
+      year: 2008,
+      cast: [
+        'Christian Bale',
+        'Heath Ledger',
+        'Aaron Eckhart',
+        'Michael Caine',
+        'Gary Oldman',
+        'Morgan Freeman',
+      ],
+    },
+    {
+      title: 'The Dark Knight Rises',
+      year: 2012,
+      cast: [
+        'Christian Bale',
+        'Gary Oldman',
+        'Tom Hardy',
+        'Michael Caine',
+        'Joseph Gordon-Levitt',
+        'Anne Hathaway',
+        'Morgan Freeman',
+        'Marion Cotillard',
+      ],
+    },
+  ];
+  const results = data.reduce((accum, next) => {
+    
+  }, [])
+}
