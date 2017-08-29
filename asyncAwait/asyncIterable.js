e14aab7752f2a1993c370a5bd4432621794c43c5
TobiahRex <tobiah.rex@gmail.com>
2017-08-29 22:36:30 +0900

    mocking out batchUpload architecture with async.

--- asyncAwait/asyncIterable.js
+++ asyncAwait/asyncIterable.js
@@ -1,6 +1,35 @@
 import axios from 'axios';
+import m from 'moment';
 
 const reports = [];
+
+conts handleErrorAndSendEmail = (error) => {
+  console.log('\nThere was an error: ', error);
+  console.log('\nSending Email now.');
+  setTimeout(() => {
+    console.log('Email has been sent.');
+  }, 400)
+}
+
+const errorReport = () => {
+  console.log('\nThere was an error: ', error);
+  console.log('\nSending Error report: ', reports.pop());
+  setTimeout(() => {
+    const reportData = reports.pop();
+    const errorReport = `
+      ERROR REPORT - ${reportData.date}
+      /-----------------------------------------/
+      Details:
+      ${reportData.message}
+    `;
+  }, 500);
+}
+
+const cleanResponse = (response) => ({
+  verified: true,
+  error: false,
+  data: response.data,
+});
 
 const fetchGithub = handle =>
 new Promise((resolve, reject) => {
@@ -11,6 +40,10 @@ new Promise((resolve, reject) => {
       resolve(response);
     } else {
       console.log('FAILED: Fetch user data for: ', handle);
+      reports.push({
+        date: m().format('YYYY/MM/DD'),
+        message: 'Was unable to fetch Github data.',
+      });
       handleErrorAndSendEmail(response.data);
       sendErrorReport();
     }
@@ -33,7 +66,13 @@ function batchUpload(argsArray) {
   .map((promise) => {
     promise
     .then((response) => {
-      const {  } = cleanResponse(data);
+      const { verified, data, error } = cleanResponse(data);
+      if (verified) {
+
+      } else {
+        handleErrorAndSendEmail(error);
+        sendErrorReport(error);
+      }
     })
     .catch((error) => {
 
