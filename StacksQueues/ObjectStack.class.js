05e7c756d01d6fed99350b259fc945ab6e5aeeb4
TobiahRex <tobiah.rex@gmail.com>
2017-09-22 13:05:25 +0900

    implemented an object style stack.

--- StacksQueues/ObjectStack.class.js
+++ StacksQueues/ObjectStack.class.js
@@ -4,13 +4,40 @@ class ObjStack {
     this._storage = {};
     this._count = 0;
   }
+  show() {
+    return console.log(this._storage);
+  }
   push(data) {
     if (this._count < this._capacity) {
       this._storage[this._count] = data;
-      return console.log(this._storage);
+      this._count += 1;
+      return this._storage;
     }
     return 'Max capacity reached. "Pop" before calling this operation.';
+  }
+  pop() {
+    if (this._count !== 0) {
+      let result = '';
+
+      this._storage = Object
+      .keys(this._storage)
+      .reduce((a, n, i, arr) => {
+        if (i !== (arr.length - 1)) {
+          a[n] = this._storage[n];
+          return a;
+        }
+        result = this._storage[n];
+        return a;
+      }, {});
+      console.log('this._storage: ', this._storage);
+      return result;
+    }
+    return 'The stack is empty.';
   }
 }
 const stack = new ObjStack(10);
 stack.push('toby');
+stack.push('bob');
+stack.show();
+stack.pop();
+stack.show();
