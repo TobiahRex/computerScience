2796e373f29c62962b65442f957228637f3c32b0
TobiahRex <tobiah.rex@gmail.com>
2017-11-24 12:29:51 -0700

    created a looping factorial function.

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -1,8 +1,17 @@
 const factorialLoop = (n) => {
   let result = 1;
-  for (let i = n; i > 0; i -= 1) result *= i;
+  for (
+    let i = n;
+    i > 0;
+    i -= 1
+  ) result *= i;
 
   return result;
 }
-factorialLoop(5) // 120
-console.log(factorialLoop(5));
+// factorialLoop(5) // 120
+
+const factorialRecursive = (n) => {
+  if (n === 1) return 1;
+  return n * factorialRecursive(n - 1);
+}
+console.log(factorialRecursive(5));
