a1c71169eaf409bd43e6af68cb44fa3e9a919fde
TobiahRex <tobiah.rex@gmail.com>
2017-11-29 14:08:00 -0700

    finished building bubbleSort.

---
+++ sort/2017/selectionSort.js
@@ -0,0 +1,37 @@
+/*
+Bubble SORT
+*** Description
+Iterate over array, comparing adjacent items and swap if in incorrect order. Largest elements bubble to the end of the array
+*** Exercises
+- Implement bubble sort
+- Identify time complexity
+Optimizations:
+- Make algorithm adaptive (if at any point array is already sorted, exit function early). After doing this, what is time complexity for nearly sorted arrays?
+- For each pass through the array, are you doing any unnecessary checking of elements? Minimize checking and consider the effect on time complexity.
+Variants:
+- Implement cocktail sort (for each pass find both min and max values and sort in both directions). How does this impact performance?
+(https://en.wikipedia.org/wiki/Cocktail_sort)
+*/
+
+const bubbleSort = (arr) => {
+  const length = arr.length - 1;
+
+  for (let i = length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      console.log('arr: ', arr);
+      let current = arr[j];
+      let next = arr[j + 1];
+      console.log('current', current);
+      console.log('next', arr[j + 1]);
+      if (current > next) {
+        arr[j] = next;
+        arr[j + 1] = current;
+      }
+      console.log('arr: ', arr);
+      console.log('/------------------------------/');
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([7, 5, 3, 1, 8, 6, 4, 2]));
