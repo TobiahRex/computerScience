d00ee805b55f8627e629668f622e3fa0079e771a
TobiahRex <tobiah.rex@gmail.com>
2018-01-27 22:52:20 -0700

    added explanation to classInheritance.py.

--- crackingCodeInterview/python/classInheritance.py
+++ crackingCodeInterview/python/classInheritance.py
@@ -1,3 +1,10 @@
+'''
+    Python uses non-parameterized constructors.
+    So even tho we articulate an argument for B & C classes,
+    [B(A), C(B)] respectively; we do not pass an argument here when creating a new class instance.
+    Calling "super()" maps the invocation to that expected Argument - the class A & B - for classes B & C, respectively.
+'''
+
 class A:
     def __init__(self):
         self.var1=5
@@ -15,6 +22,6 @@ class C(B):
 
 a = A()
 b = B()
-# c=C(b)
+c=C()
 # print(a.var1)
-print(b.var2)
+print(c.var1)
