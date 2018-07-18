0c887fdaf41ced2b2282b050437ac40d0256f208
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-17 20:59:59 -0700

    migrated LL logic into class .

--- LinkedLists/17June2018/LinkedList.js
+++ LinkedLists/17June2018/LinkedList.js
@@ -1,43 +1,41 @@
-function LinkedList() {
-  this._head = null
-  this._length = 0;
-}
+class LinkedList {
+  constructor() {
+    this._head = null;
+    this._length = 0;
+  }
 
-LinkedList.prototype.boundsCheck = (index) => {
-  if (index > -1 && index < this._length) return true;
-  return false;
-}
+  boundsCheck = (index) => {
+    if (index > -1 && index < this._length) return true;
+    return false;
+  }
 
-LinkedList.prototype.search = (index) => {
-  let current = this._head;
-  let i = 0;
-  if (index == 0) return current.data;
-  else if (LinkedList.prototype.boundsCheck(index)) {
-    while(i++ < index) {
-      current = current.next;
+  search = () => {
+    let current = this._head;
+    let i = 0;
+    if (index == 0) return current.data;
+    else if (this.boundsCheck(index)) {
+      while(i++ < index) {
+        current = current.next;
+      }
+      return current.data;
+    } else {
+      return null;
     }
-    return current.data;
-  } else {
-    return null;
+  }
+
+  createNew = (data) => {
+    const newNode = {
+      data,
+      next: null,
+    };
+
+    if (!this._head) {
+      this._head = newNode;
+    }
+    return data;
   }
 }
 
-LinkedList.prototype.insert = (data) => {
-  
-}
-
-LinkedList.prototype.createNew = (data) => {
-  const newNode = {
-    data,
-    next: null,
-  };
-
-  if (!this._head) {
-    this._head = newNode;
-  }
-  return data;
-}
-
-LinkedList()
-LinkedList.prototype.createNew("toby");
-console.log(LinkedList.prototype.search(0));
+const sll = new LinkedList();
+sll.createNew("toby");
+console.log(sll.search(0));
