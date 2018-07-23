37d65ff796245cf9805bbdffb02d35526dc67350
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-23 16:52:57 -0700

    finished building parseInputs function.

--- crackingCodeInterview/js/graphs/kth-parent/index.js
+++ crackingCodeInterview/js/graphs/kth-parent/index.js
@@ -23,14 +23,46 @@ function createGraph() {
     }
 
     _addEdge = (v1, v2) => {
-
+      this._vertices[v1].neighbors.push(v2);
+      this._vertices[v2].neighbors.push(v1);
     }
 
     _createNode = (value) => {
       return ({
         data: value,
         neighbors: Array(1),
-      })
+      });
+    }
+
+    this.parseInputs(input) {
+      const inputs = input.split('\n');
+      const testCases = Number(inputs[0]);
+      const nodes = [];
+      const queries = [];
+
+      let history = 1;
+      for (let i = 0; i < testCases; i++) {
+        let newNodes = Number(inputs[history]);
+        for (let j = 0; j < newNodes; j++) {
+          history += 1;
+
+          nodes[i].push(inputs[history]);
+        }
+        history += 1;
+
+        let newQueries = Number(inputs[history]);
+        for (let k = 0; k < newQueries; k++) {
+          history += 1;
+
+          queries[i].push(inputs[history]);
+        }
+        history += 1;
+      }
+
+      return ({
+        nodes,
+        queries,
+      });
     }
   }
 }
