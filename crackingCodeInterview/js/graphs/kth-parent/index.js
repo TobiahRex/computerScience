ccf778aa725dc2d44f94d0a397c2d1c80cc1f55c
Tobiah Rex <tobiah.rex@gmail.com>
2018-07-23 17:09:56 -0700

    continuing to build out graph implementation for kth parent.

--- crackingCodeInterview/js/graphs/kth-parent/index.js
+++ crackingCodeInterview/js/graphs/kth-parent/index.js
@@ -1,14 +1,35 @@
-const kthAncestor(input) {
-
+const kthAncestor = (input) => {
+  const graph = createGraph();
+  const inputs = graph.parseInputs(input);
+  graph.handleNodes(inputs.nodes);
+  graph.handleQueries(inputs.handleQueries);
 }
 
 function createGraph() {
-  return class Graph {
+  class Graph {
     constructor(){
       this._vertices = {};
     }
 
-    _addNode = (parent, child) => {
+    handleQueries = (queries) => {
+      queries.forEach((nodeStr) => {
+        const nodeList = nodeStr.split(' ');
+        switch(nodeList[0]) {
+          case '0': this.query_add(node_list[1], node_list[2]); break;
+          case '1': this.query_delete(node_list[1]); break;
+          case '2': this.query_print(node_list[1], node_list[2]); break;
+        }
+      });
+    }
+
+    handleNodes = (nodes) => {
+      nodes.forEach((nodeStr) => {
+        const nodeList = nodeStr.split(' ');
+        this.addNode(nodeList[0], nodeList[1]);
+      });
+    }
+
+    addNode = (parent, child) => {
       const parentNode = this._createNode(parent);
       const childNode = this._createNode(child);
 
@@ -34,7 +55,7 @@ function createGraph() {
       });
     }
 
-    this.parseInputs(input) {
+    parseInputs = (input) => {
       const inputs = input.split('\n');
       const testCases = Number(inputs[0]);
       const nodes = [];
@@ -42,20 +63,24 @@ function createGraph() {
 
       let history = 1;
       for (let i = 0; i < testCases; i++) {
-        let newNodes = Number(inputs[history]);
-        for (let j = 0; j < newNodes; j++) {
+        let newNodesLength = Number(inputs[history]);
+        let newNodes = [];
+        for (let j = 0; j < newNodesLength; j++) {
           history += 1;
 
-          nodes[i].push(inputs[history]);
+          newNodes.push(inputs[history]);
         }
+        nodes.push(newNodes);
         history += 1;
 
-        let newQueries = Number(inputs[history]);
-        for (let k = 0; k < newQueries; k++) {
+        let newQueriesLength = Number(inputs[history]);
+        let newQueries = [];
+        for (let k = 0; k < newQueriesLength; k++) {
           history += 1;
 
-          queries[i].push(inputs[history]);
+          newQueries.push(inputs[history]);
         }
+        queries.push(newQueries);
         history += 1;
       }
 
@@ -65,4 +90,35 @@ function createGraph() {
       });
     }
   }
+
+  return new Graph();
 }
+
+kthAncestor(
+  `2
+7
+2 0
+5 2
+3 5
+7 5
+9 8
+8 2
+6 8
+10
+0 5 15
+2 15 2
+1 3
+0 15 20
+0 20 13
+2 13 4
+2 13 3
+2 6 10
+2 11 1
+2 9 1
+1
+10000 0
+3
+0 10000 4
+1 4
+2 4 1`
+);
