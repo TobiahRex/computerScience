d9d78035940d6a64c44521cfd30316704271c7a1
Tobiah Rex <tobiah.rex@gmail.com>
2018-10-26 20:00:18 -0700

    upload array implementation of stringRotation.

---
+++ algorithms/cracking-coding-interview/js/arrays-strings/string-rotation-arrays.js
@@ -0,0 +1,22 @@
+const stringRotation = (s1, s2) => {
+  // match first char in s2 to first occurence in s1.
+  let i = 0;
+  while(s1[i] !== s2[0]) i++;
+  // loop over s2 up to every character
+  for (let j = 0; j < s2.length; j++) {
+    // if char in s2 !== s1, then its not a rotation.
+    if (s2[j] !== s1[i]) return false;
+    else i += 1;
+    // if i, is at the end of the s1, move i back to the front of s1.
+    if (i % s1.length == 0) i = 0;
+  }
+  // if loop finished, then we know the strings are rotations.
+  return true;
+}
+
+console.log(
+  stringRotation(
+    'waterbottle',
+    'erbo ttlewat'
+  )
+)
