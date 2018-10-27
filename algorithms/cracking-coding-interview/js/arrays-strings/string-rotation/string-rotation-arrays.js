43c8d97b2b7bddbd73b75a4b2466645d28b6d00c
Tobiah Rex <tobiah.rex@gmail.com>
2018-10-26 20:12:46 -0700

    updated comments.

--- algorithms/cracking-coding-interview/js/arrays-strings/string-rotation/string-rotation-arrays.js
+++ algorithms/cracking-coding-interview/js/arrays-strings/string-rotation/string-rotation-arrays.js
@@ -1,3 +1,13 @@
+/*
+String Rotation: Assume you have a method isSubstring which
+checks if one word is a substring of another.
+Given two strings, sl and s2,
+write code to check if s2 is a rotation of sl
+using only one call to isSubstring
+
+(e.g., "waterbottle" is a rotation of"erbottlewat").
+*/
+
 const stringRotation = (s1, s2) => {
   // match first char in s2 to first occurence in s1.
   let i = 0;
@@ -17,6 +27,6 @@ const stringRotation = (s1, s2) => {
 console.log(
   stringRotation(
     'waterbottle',
-    'erbo ttlewat'
+    'erbottlewat'
   )
 )
