cdcb46149edf8cbcf89614d1399eec6bc3e5e2dd
Tobiah Rex <tobiah.rex@gmail.com>
2018-10-26 20:24:07 -0700

    functionalized string-rotation-arrays.js

--- algorithms/cracking-coding-interview/js/arrays-strings/string-rotation/string-rotation-arrays.js
+++ algorithms/cracking-coding-interview/js/arrays-strings/string-rotation/string-rotation-arrays.js
@@ -10,15 +10,15 @@ using only one call to isSubstring
 
 const stringRotation = (s1, s2) => {
   // match first char in s2 to first occurence in s1.
-  let i = 0;
-  while(s1[i] !== s2[0]) i++;
+  let i = lookingForFirstMatch(s1, s2);
+
   // loop over s2 up to every character
   for (let j = 0; j < s2.length; j++) {
-    // if char in s2 !== s1, then its not a rotation.
-    if (s2[j] !== s1[i]) return false;
+    if (!charsAreEqual(s1, s2, i, j)) return false;
     else i += 1;
+
     // if i, is at the end of the s1, move i back to the front of s1.
-    if (i % s1.length == 0) i = 0;
+    i = verifyCounterIsGood(s1, i);
   }
   // if loop finished, then we know the strings are rotations.
   return true;
@@ -30,3 +30,19 @@ console.log(
     'erbottlewat'
   )
 )
+
+function lookingForFirstMatch(s1, s2) {
+  let i = 0;
+  while(s1[i] !== s2[0]) i++;
+  return i;
+}
+
+function charsAreEqual(s1, s2, i, j) {
+  if (s2[j] !== s1[i]) return false;
+  return true;
+}
+
+function verifyCounterIsGood(s1, i) {
+  if (i % s1.length == 0) return 0;
+  return i;
+}
