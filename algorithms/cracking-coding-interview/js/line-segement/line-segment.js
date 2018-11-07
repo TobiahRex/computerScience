5c942b6e33ba069bf456db6347efd56bc4f2cb5e
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-06 23:30:35 -0800

    buildin out line-segment.

--- algorithms/cracking-coding-interview/js/line-segement/line-segment.js
+++ algorithms/cracking-coding-interview/js/line-segement/line-segment.js
@@ -1,7 +1,14 @@
-const lineSegment (p1, p2, p3, p4) => {
+const lineSegment (p1_top, p1_low, p2_top, p2_low) => {
   //Find the y = mx + b formula for each line.
+  const slope = getSlope(args);
+  console.log('slope: ', slope);
   // Iterate through the domain of each line.
   // Save eaach x value per line, with each y value.
   // Compare the results form a memo, with each-other, and verify if
   // there is an exact pair of x and y values
+}
+lineSegment([-1, 2], [2, -1], [3, 2], [-3, -2]);
+
+function getSlope(p1_x, p1_y, p2_x, p2_y) {
+  return (p2_y - p1_y) / (p2_x, p1_x);
 }
