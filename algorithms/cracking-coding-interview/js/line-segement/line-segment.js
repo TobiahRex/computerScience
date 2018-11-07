aa646ffe0e3faebe17b1463b650629b5feb6713e
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-06 18:22:13 -0800

    building out pseudo code for line-segment and tic-tact-toe-solution.

---
+++ algorithms/cracking-coding-interview/js/line-segement/line-segment.js
@@ -0,0 +1,7 @@
+const lineSegment (p1, p2, p3, p4) => {
+  //Find the y = mx + b formula for each line.
+  // Iterate through the domain of each line.
+  // Save eaach x value per line, with each y value.
+  // Compare the results form a memo, with each-other, and verify if
+  // there is an exact pair of x and y values
+}
---
+++ algorithms/cracking-coding-interview/js/tic-tac-toe-solution/tic-tac-toe.js
@@ -0,0 +1,12 @@
+const ticTactToeSolution = (board) => {
+  // 2 loops
+  // Based on size of the board, iterate both loops, to the size of the board.
+  // Iterate across each row individually, then across each column individually.
+  // Keep a total for the row.
+  // Keep a total for player 1.
+  // keep a total for player 2.
+  // once the loop is finished, check for edge case (diagnoal solutions)
+  // Diagonal coordinates are generated dynamically but taking the pairs,
+  // of each interger (e.g. [0,0], [1,1], [2,2], [3,3]) for one diagonal.
+  // The other diagonal (e.g. [n, 0], [n-1, 0+1], [n-2, 0+2], [n-3, 0+3]);
+}
