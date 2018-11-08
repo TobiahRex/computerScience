1fbd6aea076ab143003b1fe6a19bc9b98a89c8bd
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-07 20:53:12 -0800

    modifying englishInt to use one loop.

--- algorithms/cracking-coding-interview/js/english-int/english-int.js
+++ algorithms/cracking-coding-interview/js/english-int/english-int.js
@@ -2,10 +2,29 @@
 Given any integer, print an English phrase that describes the interger, (e.g. One Thousand Two Hundred Thirty Four)
 */
 const englishInt = (n) => { // 135
-  let i = 10;
-  let results = [];
+  const values = {
+    10: 'Ten',
+    100: 'Hundred',
+    1000: 'Thousand',
+    1000000: 'Million',
+    1000000000: 'Billion',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+  };
+  let i = 10, results = [];
+
   while(n != 0) {  // n % 10
     let current = n % i; // current = 5
+    let multiplier = current.toString()[0];
+
     results.unshift(current);  // [5]
     n -= current; // n = 130
     i *= 10;
@@ -22,31 +41,10 @@ const englishInt = (n) => { // 135
  englishInt(19323);
 
 function parseSegments(seq) {
-  const values = {
-    100: 'One Hundred',
-    1000: 'One Thousand',
-    1000000: 'One Million',
-    1000000000: 'One Billion',
-    1: 'One',
-    2: 'Two',
-    3: 'Three',
-    4: 'Four',
-    5: 'Five',
-    6: 'Six',
-    7: 'Seven',
-    8: 'Eight',
-    9: 'Nine',
-    10: 'Ten',
-    20: 'Twenty',
-    30: 'Thirty',
-    40: 'Fourty',
-    50: 'Fifty',
-    60: 'Sixty',
-    70: 'Seventy',
-    80: 'Eighty',
-    90: 'Ninety',
-  }
+  const values = {}
   seq.forEach((s) => {
+    // [ 10000, 9000, 300, 20, 3 ]
+    // Three Hundred Twenty Three
     let answer = '';
   })
 }
