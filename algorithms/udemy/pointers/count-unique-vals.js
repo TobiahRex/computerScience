34c821b511bb39d30b009a2969c32b4fd7889a80
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-22 04:42:43 -0800

    finished - had a bug and fixed it.

--- algorithms/udemy/pointers/count-unique-vals.js
+++ algorithms/udemy/pointers/count-unique-vals.js
@@ -1,7 +1,7 @@
 // Array is given sorted.
 const countUnique = (array) => {
   let result = 0;
-  for (let i = 1, j = 0; i <= array.length; i++) {
+  for (let i = 0, j; i < array.length; i++) {
     if (array[j] !== array[i]) {
       result += 1;
       j = i;
@@ -11,6 +11,7 @@ const countUnique = (array) => {
 }
 console.log(
   countUnique(
-    [0, 1, 1, 1, 1, 1, 2, 2, 3, 4, 4]
+    // [0, 1, 1, 1, 1, 1, 2, 2, 4, 4, 5]
+    [0]
   )
 );
