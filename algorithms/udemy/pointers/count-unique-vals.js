3100062851f7c8dd7938becd2fe03a1aefc114ae
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-22 04:34:37 -0800

    finished.

--- algorithms/udemy/pointers/count-unique-vals.js
+++ algorithms/udemy/pointers/count-unique-vals.js
@@ -1,8 +1,15 @@
 const countUnique = (array) => {
   let result = 0;
-  for (let i = 1, j = 0; i < array.length; i++); {
-    if (array[i] !== array[j]) {
-      ``
+  for (let i = 1, j = 0; j < array.length; i++) {
+    if (array[j] !== array[i]) {
+      result += 1;
+      j = i;
     }
   }
+  return result;
 }
+console.log(
+  countUnique(
+    [0, 1, 1, 1, 1, 1, 2, 2, 3, 4, 4]
+  )
+);
