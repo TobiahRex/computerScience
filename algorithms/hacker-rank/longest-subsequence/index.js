1c680ba3beb7f68b034d6e97ec9c345639ec3d32
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-23 23:15:45 -0800

    ready to test, longest sub-sequence.

--- algorithms/hacker-rank/longest-subsequence/index.js
+++ algorithms/hacker-rank/longest-subsequence/index.js
@@ -1,5 +1,11 @@
-function longestSubsequence() {
-
+function longestSubsequence(arr) {
+  let longest = arr.length ? [arr[0]] : [];
+  for (let i = 0, j = 0; i < arr.length; i++) {
+    if (arr[i] > arr[j] && i !== j) {
+      j = i;
+      longest.push(arr[i]);
+    }
+  }
 };
 
 console.log(
@@ -12,8 +18,9 @@ console.log(
 15
 15, 27,
 15, 27, 38
+15, 27, 38, 55
 
-      i
+                      i
 [15, 27, 14, 38, 26, 55, 46, 65, 85]
-  k
+                      k
 */
