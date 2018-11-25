cb13696e27ea870fc17c48fbce7a1a1cd22eb15d
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-24 22:17:37 -0800

    finished writing geometric progression algorithm.

---
+++ algorithms/geeks-for-geeks/series-gp/index.js
@@ -0,0 +1,20 @@
+function seriesGP(inputs, iterations) {
+  if (iterations == 1 || iterations == 2) return inputs[iterations - 1];
+
+  let ratio = inputs[1] / inputs[0],
+    count = 2;
+    answer = inputs[1];
+  while(count++ < iterations) {
+    answer *= ratio;
+  }
+  return Math.floor(answer);
+}
+
+(() => {
+  console.log(
+    seriesGP(
+      [2, 3],
+      4
+    )
+  )
+})();
