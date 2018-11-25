0fd1d2673e743626c51e6f8250d8b60b6d137184
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-25 07:25:39 -0800

    finished writing "sum-is-palindrome"(c++).

---
+++ algorithms/geeks-for-geeks/sum-is-palindrome/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string isPalindrome(int);
+int sumDigits(int);
+
+int main() {
+  int nums[] = {
+    111,
+    121,
+    9999991,
+    89,
+  };
+  for (int n : nums) {
+    int sum = sumDigits(n);
+    cout << isPalindrome(sum) << endl;
+  }
+
+  return 0;
+}
+
+int sumDigits(int n) {
+  string numStr = to_string(n);
+  int total{0};
+  for (char s : numStr) total += s - '0';
+  return total;
+}
+
+string isPalindrome(int sum) {
+  string numStr = to_string(sum);
+  string answer("YES");
+
+  if (numStr.length() === 1) return answer;
+
+  for (int i = 0; j = numStr.length() - 1; i < j; i++, j--) {
+    if (numStr[i] !== numStr[j]) {
+      answer = "NO";
+      break;
+    }
+  }
+  return answer;
+}
