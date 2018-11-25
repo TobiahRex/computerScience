fce51709216ea5b278285a6cd3982f184530adec
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-24 17:12:32 -0800

    finished "printTable".

--- algorithms/geeks-for-geeks/printTable/index.js
+++ algorithms/geeks-for-geeks/printTable/index.js
@@ -1,10 +1,24 @@
-function printTable(n) {
-
+function printTable(arr) {
+  let lastProduct = '';
+  for (let i = 0; i < arr[0]; i++) {
+    for (let j = 1; j <= 10; j++) {
+      lastProduct += ' ' + String(arr[1] * j);
+    }
+    lastProduct += '\n';
+  }
+  return lastProduct;
 }
 
 function main() {
-  let testCases = [[2, 8], [2, 3], [2, 5], [2, 2]];
+  let testCases = [
+    [6, 9],
+    [2, 3],
+    [2, 5],
+    [2, 2]
+  ];
+  // let testCases = [[2, 8], [2, 3], [2, 5], [2, 2]];
   testCases.forEach((n) => {
-    printTable(n);
+    console.log(printTable(n));
   });
 }
+main();
