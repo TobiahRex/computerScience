e6fb76bd4efc3dfad2caeec5f4638a249a6a59b3
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-25 12:47:36 -0800

    finished psuedo-coding jumpingNumbers.

--- algorithms/geeks-for-geeks/jumping-number/index.js
+++ algorithms/geeks-for-geeks/jumping-number/index.js
@@ -15,32 +15,16 @@ const increaseNum = (num) => {
 function jumpingNumber(numberStr) {
   const digits = new Array(numberStr.length).fill(0);
   let targetNum = Number(numberStr),
-    keepAdding = true,
-    sum = 0;
+    keepAdding = true;
 
   while(keepAdding) {
-    increaseDigits(sum, digits);
+    increaseDigits(digits, digits.length - 1);
     if (sum > targetNum) keepAdding = false;
   }
 }
 
-function increaseDigits(sumRef, digitsRef) {
-  let increase = true, currentIndex = digitsRef.length - 1;
+function increaseNum(digitsRef) {
 
-  while(increase) {
-    smaller = getNextN(digitsRef[currentIndex]).newValue;
-    bigger = getNextN(digitsRef[currentIndex - 1]).newValue;
-    if (smaller === bigger) {
-      smaller = getNextN(digitsRef[currentIndex]).newValue;
-    }
-
-    if (getNextN(digitsRef[currentIndex]).tooLarge) {
-      currentIndex - 1;
-      if ()
-    } else {
-      increase = false;
-    }
-  }
 }
 
 processData(
