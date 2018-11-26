46c16866470857718940add00f6ce83c9b61e8b9
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-26 12:06:45 -0800

    finished with nPr.

--- algorithms/geeks-for-geeks/nPr/index.js
+++ algorithms/geeks-for-geeks/nPr/index.js
@@ -1,3 +1,19 @@
 const processData = (inputs) =>
   inputs.split('\n').forEach((n, i) =>
-  )
+  i > 0 && console.log(nPr(n)));
+
+function nPr(input) {
+  let nums = input.split(' ').map((n) => parseInt(n));
+  return (factorial(nums[0])/(factorial(nums[0]-nums[1])));
+}
+
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return (n * factorial(n - 1));
+}
+
+processData(
+`2
+2 1
+10 4`
+)
