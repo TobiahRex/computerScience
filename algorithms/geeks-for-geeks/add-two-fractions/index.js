1662f790ad48ab5acf13b2d5076d3f9b0c092079
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-25 23:07:47 -0800

    almost finished wtih "add-two-fractions".

--- algorithms/geeks-for-geeks/add-two-fractions/index.js
+++ algorithms/geeks-for-geeks/add-two-fractions/index.js
@@ -1,3 +1,21 @@
 const processData = (inputs) => inputs.split('\n').forEach((n, i) => i > 0 && console.log(addFractions(n)));
 
-function addFractions()
+function addFractions(inputStr) {
+  // find which number has the largest denominator.
+  // multiple the other numbers numberator by the divisor.
+  let inputs = inputStr.split(' ').map((n) => parseInt(n));
+  let commDenom = lcm(inputs[1], inputs[3]);
+  let newN1 = inputs[0] * (commDenom / inputs[1]);
+  let newN2 = inputs[0] * (commDenom / inputs[1]);
+  let nx = newN1 + newN2;
+  return `${nx}/${commDenom}`;
+}
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+function gcd(a, b) {
+  if (b === 0) return a;
+  return gcd(b, a % b);
+}
