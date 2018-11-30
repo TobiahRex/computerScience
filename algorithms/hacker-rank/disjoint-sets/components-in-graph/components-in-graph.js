8cb7503a6e5fd39b4a17b16e06d4033cb02bd2e1
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-30 08:02:52 -0800

    building out disjoint sets still.

--- algorithms/hacker-rank/disjoint-sets/components-in-graph/components-in-graph.js
+++ algorithms/hacker-rank/disjoint-sets/components-in-graph/components-in-graph.js
@@ -1,59 +1,64 @@
 const processData = (input) => {
-  input.split('\n').slice(1).reduce(disjointSet, { setActions: universalSet() });
+  input.split('\n').slice(1).reduce(disjointSet, { setActions: createSet() });
 };
 
-function universalSet() {
+function createSet() {
   let u = {};
   let sets = [];
 
   return ({
-    addToSet(a, b) {
-      if (b === null) {
-        u[a] = true;
-        return;
-      }
-
-
-    }
-    add(value){
-      if (!(value in u)){
-        u[value] = true;
-
+    createSet(a, b) {
+      sets.push({ [a]: true, [b]: true });
+    },
+    addToSet(child, parent) {
+      if (parent === null) u[child] = true;
+      else {
+        // locate parent set in sets | "parent" = index of the set.
+        sets[parent][child] = true;
       }
     },
     getSets(g, b){
-      let gSet, bSet;
+      let gSet = -1, bSet = -1;
 
       sets.forEach((set, i) => {
         if (g in set) gSet = i
-        else if(!getSet) gSet = -1;
-
         if (b in set) bSet = i
-        else if (!bSet) bSet = -1;
       });
-
-      return {
-        gSet: u[g] || -1,
-        bSet: u[b] || -1
-      };
+      return { gSet, bSet };
     },
   });
 }
 
-function disjointSet(acc, input) {
-  let [g, b] = input.split(' ');
-  let setActions = universalSet(),
-  { gSet, bSet } = setActions.getSets(g, b);
+function disjointSet(acc, input, i) {
+  [g, b] = input.split(' ');
+  let { gSet, bSet } = acc.setActions.getSets(g, b);
 
   if (gSet) {
+    // both g & b are already assigned to a set
     if (bSet) {
+      // if their sets are not the same, perform union.
       if (gSet !== bSet) setActions.union(gSet, bSet);
-      else // do nothing, its a repeat (probably will never happen.)
+      // else // do nothing, its a repeat (probably will never happen.)
     } else {
       // add B to universal set.
       setActions.addToSet(bSet, null);
-      // add B to G set.
-      setActions.addToSet(bSet, aSet);
+      // add B to G (the parent of B).
+      setActions.addToSet(bSet, gSet);
+    }
+  } else {
+    // g is not in the universal set.
+    setActions.addToSet(g, null);
+    // g does not already have a set.
+    if (!bSet) {
+      // b also does not already have a set.
+
+      // a & b are not in the universal set already so add them.
+      setActions.addToSet(b, null);
+      setActions.createSet(g, b);
+    } else {
+      // g is not in the u set, but be is.
+      // so add g to b.
+      setActions.addToSet(g, b);
     }
   }
 }
@@ -61,9 +66,9 @@ function disjointSet(acc, input) {
 // look to see if g and b are already in a set.
 
 processData(
-  `5
-  1 6
-  2 7
-  3 8
-  4 9
-  2 6`);
+`5
+1 6
+2 7
+3 8
+4 9
+2 6`);
