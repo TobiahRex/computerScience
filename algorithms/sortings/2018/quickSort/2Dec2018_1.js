0eeec58af20a299881b00ab95dfbb479eb518cdf
Tobiah Rex <tobiah.rex@gmail.com>
2018-12-02 14:54:25 -0800

    finished building out quickSort from memory.

---
+++ algorithms/sortings/2018/quickSort/2Dec2018_1.js
@@ -0,0 +1,35 @@
+(() => {
+  let arr = [20,14,18,15,11,17,16,12,13,19,3,5,8,1,9,7,6,0,2,4];
+  quickSort(arr);
+  console.log(arr);
+})();
+
+function quickSort(arr, lo = 0, hi = arr.length - 1) {
+  if (lo > hi || lo === hi) return;
+
+  let p = partition(arr, lo, hi);
+  quickSort(arr, lo, p);
+  quickSort(arr, p + 1, hi);
+}
+
+function partition(arr, lo, hi) {
+  let iNext = lo,
+    iPartition = hi,
+    sort = true;
+
+  while(sort) {
+    if (arr[iNext] > arr[iPartition]) {
+      swap(arr, iNext, iPartition);
+      swap(arr, iPartition, iPartition + 1);
+      iPartition--;
+    } else if (iNext === iPartition) sort = false;
+    else iNext++
+  }
+  return iPartition;
+}
+
+function swap(arr, iA, iB) {
+  let temp = arr[iA];
+  arr[iA] = arr[iB];
+  arr[iB] = temp;
+}
