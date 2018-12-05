2acdb843b3213af44925599069e3844943a128b3
Tobiah Rex <tobiah.rex@gmail.com>
2018-12-04 23:27:46 -0800

    finished building palindrome sliding window.

--- algorithms/array-string-manipulation/sigma/palindrome-subsequence/main.js
+++ algorithms/array-string-manipulation/sigma/palindrome-subsequence/main.js
@@ -1,6 +1,6 @@
 (() => {
-  let string = 'tobyrextoby';
-  getScore(string);
+  let string = 'bobybob';
+  console.log(getScore(string));
 })();
 
 function getScore(str) {
@@ -10,24 +10,24 @@ function getScore(str) {
     matrix[i] = new Array(len).fill(0);
     matrix[i][i] = 1;
   }
-  console.log(matrix)
+  matrix
   // create a sliding-window
   // dynamically grow the size of the window
-  for (let window = 2; window <= len; window++) {
-    // for each window,
-    // compare the left and right-bound values of the window
-    for (let i = 0, j; i < (len - window + 1); i++) {  // +1 due to the right-bound of the window
+  for (let sWindow = 2; sWindow <= len; sWindow++) {
+    sWindow
+    // for each sWindow,
+    // compare the left and right-bound values of the sWindow
+    for (let i = 0, j = 0; i < (len - sWindow + 1); i++) {  // +1 due to the right-bound of the sWindow
       // calculate the right-bound index value
-      j = (len - window) - 1  // the current left-bound value + the window size, minus 1 for 0-based index.
-      if (str[i] === str[j] && window === 2) { // if the very first 2 values are equal, incrememnt by 1 instead of the normal 2
-        matrix[i][j] = 2
+      j = (i + sWindow) - 1  // the current left-bound value + the sWindow size, minus 1 for 0-based index.
+      if (str[i] === str[j] && sWindow === 2) { // if the very first 2 values are equal, incrememnt by 1 instead of the normal 2
+        matrix[i][j] = 2;
       } else if (str[i] === str[j]) { // assign as sum of bottom-left diagonal value and 2
         matrix[i][j] = matrix[i + 1][j - 1] + 2;
       } else { // else, take the larger of the bottom or left value.
-        matrix[i][j] = Math.max(str[i + 1][j], str[i][j - 1]);
+        matrix[i][j] = Math.max(matrix[i + 1][j], matrix[i][j - 1]);
       }
     }
   }
-  console.log(matrix)
   return matrix[0][len - 1]; // return the right-most matrix value since it is the largest count.
 }
