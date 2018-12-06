6feb9f1c58683341e419b1ead0a68c25607c9d16
Tobiah Rex <tobiah.rex@gmail.com>
2018-12-05 21:52:08 -0800

    building out 1.js.

--- algorithms/geeks-for-geeks/palindromes/1.js
+++ algorithms/geeks-for-geeks/palindromes/1.js
@@ -1,27 +1,13 @@
 (() => {
-  let input = [3,3,2,1,3,4,4,4];
+  // let input = [1];
+  let input = [35,65,69,28,12,69,37,80,80,50,80,50,15,57,79,69,57,65,15,69,57,50,65,2,14,64,15,65,65,5,15,64,57,37,50,12,64,37,28,20,80,80,50];
   let answer = equalizeArray(input);
   answer
 })();
 
-// {
-//   3: {
-//     count: 3,
-//     index: [0, 1, 3],
-//   },
-//   2: {
-//     count: 1,
-//     index: [2]
-//   },
-//   1: {
-//     count: 1,
-//     index: [3]
-//   },
-// }
-
-
 function equalizeArray(arr) {
   let maxStack = [], max = 0, memo = {};
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] in memo) {
       memo[arr[i]].count += 1;
@@ -39,10 +25,37 @@ function equalizeArray(arr) {
       };
     }
   }
-  maxStack
+  console.log(maxStack)
   let saveLength = 0;
   maxStack.forEach((obj) => {
     saveLength += obj.index.length;
   });
   return arr.length - saveLength;
+}
+
+const mergeSort(arr, lo, hi) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+    leftArray = arr.slice(0, mid),
+    rightArray = arr.slice(mid);
+
+  let leftSorted = mergeSort(leftArray, mid),
+    rightSorted = mergeSort(rightArray, mid + 1);
+
+  return merge(leftSorted, rightSorted);
+}
+
+function merge(left, right) {
+  let result = [],
+    i = 0,
+    j = 0,
+    k = 0;
+
+  while(i < left.length && j < right.length) {
+    if(left[i] < right[i]) result[k++] = left[i++];
+    else result[k++] = right[j++];
+  }
+  for(; i < left.length; i++) result[k++] = left[i];
+  for(; j < right.length; j++) result[k++] = right[j];
+  return result;
 }
