0f8eae492d29caf5e86a8b437210087076e0bbb3
Tobiah Rex <tobiah.rex@gmail.com>
2018-12-05 20:58:44 -0800

    refactring equalizeArray.

--- algorithms/geeks-for-geeks/palindromes/1.js
+++ algorithms/geeks-for-geeks/palindromes/1.js
@@ -3,20 +3,26 @@
   equalizeArray(input);
 })();
 
-function equalizeArray(arr) {
-  let hashMap = [];
-  for (let i = 0; i < arr.length; i++)
-    arr[i] in dictionary ? dictionary[arr[i]] += 1 : dictionary[arr[i]] = 1;
-
-  dictionary
-  let max = Math.max(...Object.keys(dictionary));
-  let other = Object.keys(dictionary).filter((key) => Number(key) !== max);
-  // Save the occurences of each numbers.
-  // count all the number occurences that are not the max.
-  let matrix = new Array(arr.length).fill(0);
-  for (let i = 0;)
+{
+  3: {
+    count: 3,
+    index: [0, 1, 3],
+  },
+  2: {
+    count: 1,
+    index: [2]
+  },
+  1: {
+    count: 1,
+    index: [3]
+  },
 }
 
-function hash(val) {
-  for (let i = 0; i < String())
+[3];
+
+function equalizeArray(arr) {
+  let stack = [];
+  for (let i = 0; i < arr.length; i++) {
+
+  }
 }
